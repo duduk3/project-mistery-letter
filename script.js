0a5bbe917e9dee50fd8770a51857718036043e28
EDUARDO FERREIRA <dupearljam@gmail.com>
2021-12-29 15:54:00 -0300

    add funçao que verifica se foi preenchido o input

---
+++ script.js
@@ -0,0 +1,28 @@
+/* eslint-disable sonarjs/cognitive-complexity */
+/* eslint-disable no-undef */
+/* eslint-disable no-use-before-define */
+/* eslint-disable space-before-blocks */
+/* eslint-disable editorconfig/editorconfig */
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-const */
+let textInput = document.querySelector('#carta-texto');
+let btnCarta = document.querySelector('#criar-carta');
+let cartaGerada = document.querySelector('#carta-gerada');
+
+
+btnCarta.addEventListener('click', gerarCarta);
+
+function gerarCarta(){
+    let numCaracteres = textInput.value;
+    let count = 0;
+    for (let i = 0; i < numCaracteres.length; i += 1) {
+      if (numCaracteres[i] === ' ') {
+          count += 1;
+      }
+    }
+      if (numCaracteres.length === count || textInput.value === '') {
+          cartaGerada.innerText = "Por favor, digite o conteúdo da carta.";
+      }
+  console.log(numCaracteres.length);
+}
